2c3aebb11730cff27e6e7e809fb74377a188d846
Tomek Karwowski <to.karwowski@gmail.com>
2022-03-02 23:10:56 +0100

    add button with copyable link to game session

--- js/helper.js
+++ js/helper.js
@@ -1,10 +1,17 @@
 import * as wasm from "../pkg/footballers";
 
+const sessionIdParagraph = document.getElementById('session_id_paragraph');
 const idParagraph = document.getElementById('session_id');
+const gameLinkButton = document.getElementById('game_link_button');
 const buttons = document.getElementById('buttons');
 const startButton = document.getElementById('start_button');
 const joinButton = document.getElementById('join_button');
 const idInput = document.getElementById('id_input');
+
+async function copyToClipboard(sessionId) {
+    const link = `${window.location.href}?session_id=${sessionId}&is_host=false`;
+    await navigator.clipboard.writeText(link);
+}
 
 const params = new URLSearchParams(window.location.search);
 if (params.has("session_id")) {
@@ -12,15 +19,22 @@ if (params.has("session_id")) {
     wasm.main(params.get("session_id"), params.get("is_host") === "true");
 } else {
     startButton.addEventListener('click', event => {
-        buttons.style.visibility = "hidden";
         let sessionId = wasm.get_random_session_id();
+        buttons.style.visibility = "hidden";
+        sessionIdParagraph.hidden = false;
         idParagraph.innerText = sessionId;
+        gameLinkButton.hidden = false;
+        gameLinkButton.onclick = () => copyToClipboard(sessionId);
         wasm.main(sessionId, true);
     });
 
     joinButton.addEventListener('click', event => {
-        buttons.style.visibility = "hidden";
         let sessionId = idInput.value;
+        buttons.style.visibility = "hidden";
+        sessionIdParagraph.hidden = false;
+        idParagraph.innerText = sessionId;
+        gameLinkButton.hidden = false;
+        gameLinkButton.onclick = () => copyToClipboard(sessionId);
         wasm.main(sessionId, false);
     });
 }
